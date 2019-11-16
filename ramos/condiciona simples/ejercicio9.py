0aa25a82cf5e9b77993f6974c2ecf239b15a39c4
ramos tito <yramost@unprg.edu.pe>
2019-11-16 11:02:53 -0500

    se agrego el noveno ejercicio

---
+++ ramos/condiciona simples/ejercicio9.py
@@ -0,0 +1,31 @@
+import os
+#mostrar valores
+cliente, precio, cantidad_producto, monto_total="", 0.0, 0, 0.0
+
+#input
+cliente=os.sys.argv[1]
+precio=float(os.sys.argv[2])
+cantidad_producto=float(os.sys.argv[3])
+monto_total=float(os.sys.argv[4])
+
+#processing
+monto_pagar=(precio*cantidad_producto)
+
+#verificador
+cliente_diamante=(monto_pagar>500)
+
+#output
+print("##################################")
+print("voleta electronica de voltaje")
+print("###############################")
+print("cliente:",cliente)
+print("#####################################")
+print("precio:",precio)
+print("####################")
+print("monto a pagar=",monto_pagar)
+
+#condicional simple
+if (cliente_diamante==True):
+    print("usted tendra usted tendra un descuento especial")
+
+#fin-if
