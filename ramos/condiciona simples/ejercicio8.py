62d5c78089e8749bbdb429ca69a89738f3fcc8ae
ramos tito <yramost@unprg.edu.pe>
2019-11-16 11:01:27 -0500

    se agrego el octavo ejercicio

---
+++ ramos/condiciona simples/ejercicio8.py
@@ -0,0 +1,28 @@
+import os
+#mostrar valores
+masa, gravedad =0.0 ,0.0
+
+#input
+masa=float(os.sys.argv[1])
+gravedad=float(os.sys.argv[2])
+
+#processing
+peso=(masa * gravedad)
+
+#verificador
+peso_saludable=(peso<70)
+
+#output
+print("##################################")
+print("voleta electronica de peso")
+print("###############################")
+print("masa:",masa)
+print("#####################################")
+print("gravedad:",gravedad)
+print("####################")
+print("peso:",peso)
+
+#condicion simple
+if(peso_saludable==False):
+    print("esta mal de salud")
+
