ef0040cc3e28cfecc2e9dc92a3d5b9c914fd39c0
ramostitoyostin-unprg <57698982+ramostitoyostin-unprg@users.noreply.github.com>
2019-11-16 10:58:12 -0500

    se agrego el sexto ejercicio

---
+++ ramos/condiciona simples/ejercicio6.py
@@ -0,0 +1,35 @@
+import os
+#mostrar valores
+sueldo_empleado1, sueldo_empleado2, sueldo_empleado3, sueldo_mensual=0.0, 0.0, 0.0, 0.0
+
+#input
+sueldo_empleado1=float(os.sys.argv[1])
+sueldo_empleado2=float(os.sys.argv[2])
+sueldo_empleado3=float(os.sys.argv[3])
+sueldo_mensual=float(os.sys.argv[4])
+
+#processing
+sueldo_total=(sueldo_empleado1+sueldo_empleado2+sueldo_empleado3)
+
+#verificador
+empleados_eficientes=(sueldo_total>20000)
+
+#output
+
+print("##################################")
+print("voleta electronica de sueldo")
+print("###############################")
+print("sueldo:",sueldo_mensual)
+print("#####################################")
+print("el sueldo del empleado1:",sueldo_empleado1)
+print("el sueldo del empleado2:",sueldo_empleado2)
+print("el sueldo del empleado3",sueldo_empleado3)
+print("####################")
+print("sueldo total=",sueldo_total)
+
+#condicion simple
+#si los empleados son eficientes entonces ganaran un viaje
+if (empleados_eficientes==True):
+    print("entoces se iran de viaje")
+
+#fin_if
